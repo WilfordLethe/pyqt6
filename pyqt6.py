06c26283018e6d65fe34b40f77d5424842308012
Wilford <WilfordLethe@outlook.com>
2025-03-07 17:16:50 +0800

    review

--- pyqt6.py
+++ pyqt6.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python
-# file: calculator.py
+# file: review.py
 
 """
 ZetCode PyQt6 tutorial
 
-In this example, we create a skeleton
-of a calculator using QGridLayout.
+In this example, we create a bit
+more complicated window layout using
+the QGridLayout manager.
 
 Author: Jan Bodnar
 Website: zetcode.com
 """
 
 import sys
-from PyQt6.QtWidgets import (QWidget, QGridLayout,
-        QPushButton, QApplication)
+from PyQt6.QtWidgets import (QWidget, QLabel, QLineEdit,
+        QTextEdit, QGridLayout, QApplication)
 
 
 class Example(QWidget):
@@ -26,27 +27,30 @@ class Example(QWidget):
 
     def initUI(self):
 
+        title = QLabel('Title')
+        author = QLabel('Author')
+        review = QLabel('Review')
+
+        titleEdit = QLineEdit()
+        authorEdit = QLineEdit()
+        reviewEdit = QTextEdit()
+
         grid = QGridLayout()
+        grid.setSpacing(10)
+
+        grid.addWidget(title, 1, 0)
+        grid.addWidget(titleEdit, 1, 1)
+
+        grid.addWidget(author, 2, 0)
+        grid.addWidget(authorEdit, 2, 1)
+
+        grid.addWidget(review, 3, 0)
+        grid.addWidget(reviewEdit, 3, 1, 5, 1)
+
         self.setLayout(grid)
 
-        names = ['Cls', 'Bck', '', 'Close',
-                 '7', '8', '9', '/',
-                 '4', '5', '6', '*',
-                 '1', '2', '3', '-',
-                 '0', '.', '=', '+']
-
-        positions = [(i, j) for i in range(5) for j in range(4)]
-
-        for position, name in zip(positions, names):
-
-            if name == '':
-                continue
-
-            button = QPushButton(name)
-            grid.addWidget(button, *position)
-
-        self.move(300, 150)
-        self.setWindowTitle('Calculator')
+        self.setGeometry(300, 300, 350, 300)
+        self.setWindowTitle('Review')
         self.show()
 
 

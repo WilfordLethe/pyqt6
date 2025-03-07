2b6613d5f0dfab6a1a68a489aab4b64e74e27d5b
Wilford <WilfordLethe@outlook.com>
2025-03-07 15:30:42 +0800

    tooltip

--- pyqt6.py
+++ pyqt6.py
@@ -1,32 +1,49 @@
 #!/usr/bin/python
-# file: simple.py
+# file: tooltip.py
 
 """
 ZetCode PyQt6 tutorial
 
-In this example, we create a simple
-window in PyQt6.
+This example shows a tooltip on
+a window and a button.
 
 Author: Jan Bodnar
 Website: zetcode.com
 """
 
-
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import (QWidget, QToolTip,QPushButton, QApplication)
+from PyQt6.QtGui import QFont
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+
+    def initUI(self):
+
+        QToolTip.setFont(QFont('SansSerif', 10))
+
+        self.setToolTip('This is a <b>QWidget</b> widget')
+
+        btn = QPushButton('Button', self)
+        btn.setToolTip('This is a <b>QPushButton</b> widget')
+        btn.resize(btn.sizeHint())
+        btn.move(50, 50)
+
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle('Tooltips')
+        self.show()
 
 
 def main():
 
     app = QApplication(sys.argv)
-
-    w = QWidget()
-    w.resize(250, 200)
-    w.move(300, 300)
-
-    w.setWindowTitle('Simple')
-    w.show()
-
+    ex = Example()
     sys.exit(app.exec())
 
 

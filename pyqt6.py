1e77816cbd58ea26c299b708655e36faf0a97404
Wilford <WilfordLethe@outlook.com>
2025-03-07 16:41:28 +0800

    simple_menu

--- pyqt6.py
+++ pyqt6.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
-# file: statusbar.py
+# file: simple_menu.py
 
 """
 ZetCode PyQt6 tutorial
 
-This program creates a statusbar.
+This program creates a menubar. The
+menubar has one menu with an exit action.
 
 Author: Jan Bodnar
 Website: zetcode.com
@@ -12,6 +13,7 @@ Website: zetcode.com
 
 import sys
 from PyQt6.QtWidgets import QMainWindow, QApplication
+from PyQt6.QtGui import QIcon, QAction
 
 
 class Example(QMainWindow):
@@ -24,10 +26,19 @@ class Example(QMainWindow):
 
     def initUI(self):
 
-        self.statusBar().showMessage('Ready')
+        exitAct = QAction(QIcon('exit.png'), '&Exit', self)
+        exitAct.setShortcut('Ctrl+Q')
+        exitAct.setStatusTip('Exit application')
+        exitAct.triggered.connect(QApplication.instance().quit)
+
+        self.statusBar()
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAct)
 
         self.setGeometry(300, 300, 350, 250)
-        self.setWindowTitle('Statusbar')
+        self.setWindowTitle('Simple menu')
         self.show()
 
 

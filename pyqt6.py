48943201c7b1c0778e981db3317e9051eea345f5
Wilford <WilfordLethe@outlook.com>
2025-03-10 16:04:18 +0800

    draw_points

--- pyqt6.py
+++ pyqt6.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python
-# file: draw_text.py
+# file: draw_points.py
 
 """
 ZetCode PyQt6 tutorial
 
-In this example, we draw text in Russian Cylliric.
+In the example, we draw randomly 1000 red points
+on the window.
 
 Author: Jan Bodnar
 Website: zetcode.com
 """
 
-import sys
 from PyQt6.QtWidgets import QWidget, QApplication
-from PyQt6.QtGui import QPainter, QColor, QFont
+from PyQt6.QtGui import QPainter
 from PyQt6.QtCore import Qt
+import sys, random
 
 
 class Example(QWidget):
@@ -26,26 +27,30 @@ class Example(QWidget):
 
     def initUI(self):
 
-        self.text = "Лев Николаевич Толстой\nАнна Каренина"
-
+        self.setMinimumSize(50, 50)
         self.setGeometry(300, 300, 350, 300)
-        self.setWindowTitle('Drawing text')
+        self.setWindowTitle('Points')
         self.show()
 
 
-    def paintEvent(self, event):
+    def paintEvent(self, e):
 
         qp = QPainter()
         qp.begin(self)
-        self.drawText(event, qp)
+        self.drawPoints(qp)
         qp.end()
 
 
-    def drawText(self, event, qp):
+    def drawPoints(self, qp):
 
-        qp.setPen(QColor(168, 34, 3))
-        qp.setFont(QFont('Decorative', 10))
-        qp.drawText(event.rect(), Qt.AlignmentFlag.AlignCenter, self.text)
+        qp.setPen(Qt.GlobalColor.red)
+        size = self.size()
+
+        for i in range(1000):
+
+            x = random.randint(1, size.width() - 1)
+            y = random.randint(1, size.height() - 1)
+            qp.drawPoint(x, y)
 
 
 def main():
